22db25c547db77483f25d4309d943166b01c4b77
atif-hanif <94978139+atif-hanif@users.noreply.github.com>
2024-10-08 03:39:08 -0700

    08-10-2024 (2)

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,6 @@ function formatCrypto (state) {
   return $state;
 }
 
-
 $(document).ready(function() {
     $('.cryptocurrency').select2({
         templateResult: formatCrypto,
@@ -46,26 +45,29 @@ d3.csv("currencies.csv").then(data => {
 		.attr("width", width)
 		.attr("height", height);
 
-	// Create bubbles group elements, initially placed at random positions inside the canvas
-	const bubbles = svg.selectAll(".bubble")
+	// Create squares group elements, initially placed at random positions inside the canvas
+	const squares = svg.selectAll(".square")
 		.data(root.descendants().slice(1))  // Remove the root node
 		.enter()
 		.append("g")
-		.attr("class", "bubble")
+		.attr("class", "square")
 		.attr("transform", d => `translate(${Math.random() * (width - padding * 2) + padding}, ${Math.random() * (height - padding * 2) + padding})`);  // Start from random positions within bounds
 
 	// Append circles and animate them from random positions to final positions
-	bubbles.append("circle")
-		.attr("r", d => d.r)  // Set the correct radius right away
+	squares.append("rect")
+		.attr("width", d => d.r * 2)  // Set width based on the radius
+		.attr("height", d => d.r * 2)  // Set height based on the radius
+		.attr("x", d => -d.r)  // Center the square horizontally
+		.attr("y", d => -d.r)  // Center the square vertically
 		.attr("fill", d => colorScale(d.data.Currency));
 
 	// Animate the movement of circles to their final positions
-	bubbles.transition()
+	squares.transition()
 		.duration(2000)  // Animate the movement over 2 seconds
 		.attr("transform", d => `translate(${d.x + padding}, ${d.y + padding})`);
 
 	// Append text labels for currency and rates
-	bubbles.append("text")
+	squares.append("text")
 		.attr("dy", "-0.4em")
 		.style("text-anchor", "middle")
 		.append("tspan")
